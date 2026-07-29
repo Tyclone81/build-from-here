67cd3de5e8003e70276f2d331818c7f7d57918c4
vogero <ogerovictor81@gmail.com>
2026-07-29 16:20:05 +0300

    add printmemory file with the instructions of what the program is required to do

---
+++ printmemory/printmemory.go
@@ -0,0 +1,12 @@
+//Write a function that takes (arr [10]byte), and displays the memory as in the example.
+//After displaying the memory the function must display all the ASCII graphic characters. The non printable characters must be replaced by a dot.
+//The ASCII graphic characters are any characters intended to be written, printed, or otherwise displayed in a form that can be read by humans, present on the ASCII encoding.
+package main
+import "fmt"
+func PrintMemory(arr [10]byte) {
+
+}
+
+func main() {
+	piscine.PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+}
