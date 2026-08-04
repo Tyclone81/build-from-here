56901865827949db4ab6eaa2ec26e85b52b84849
vogero <ogerovictor81@gmail.com>
2026-08-04 17:24:15 +0300

    feat: firstword function declaration

---
+++ firstword/firstword.go
@@ -0,0 +1,7 @@
+/* Write a function that takes a string and return a string containing its first word, followed by a newline ('\n').
+A word is a sequence of characters delimited by spaces or by the start/end of the argument. */
+package main
+import "fmt"
+func FirstWord(s string) string {
+    
+}
