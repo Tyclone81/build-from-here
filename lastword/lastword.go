e17758d1253c0609fba95d3a6c81657f11b2a53b
vogero <ogerovictor81@gmail.com>
2026-08-04 17:04:09 +0300

    feat: lastword function declaration

---
+++ lastword/lastword.go
@@ -0,0 +1,7 @@
+/* Write a function LastWord that takes a string and returns its last word followed by a \n.
+A word is a section of string delimited by spaces or by the start/end of the string. */
+package main
+import "fmt"
+func LastWord(s string) string{
+	
+}
