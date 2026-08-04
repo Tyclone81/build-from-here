3ac23a3972cb5f7f0b484b77cf0082cdbe66525e
vogero <ogerovictor81@gmail.com>
2026-08-04 17:32:49 +0300

    fix comment to the firstword logic

--- firstword/firstword.go
+++ firstword/firstword.go
@@ -3,17 +3,23 @@ A word is a sequence of characters delimited by spaces or by the start/end of th
 package main
 import "fmt"
 func FirstWord(s string) string {
+
+	// set i to the index of the first character in the string
     i := 0
+
+	//skip all leading spaces by moving forward
 	for i < len(s) && s[i] == ' ' {
 		i++
 	}
 	if i >= len (s) {
 		return "\n"
 	}
+	// mark where the first word starts
 	start := i
 	for i < len(s) && s[i] != ' ' {
 		i++
 	}
+	// slice from 1st character of word to one character past the last character of the word
 	return s[start:i] + "\n"
 }
 
