989fbf9dcebd57a1e9761462b25e414b493d4e7d
vogero <ogerovictor81@gmail.com>
2026-08-04 17:19:46 +0300

    implement comments to the lastword logic

--- lastword/lastword.go
+++ lastword/lastword.go
@@ -3,17 +3,25 @@ A word is a section of string delimited by spaces or by the start/end of the str
 package main
 import "fmt"
 func LastWord(s string) string{
+
+	// set i to the index of the last character in the string
 	i := len(s)-1
+
+	// move i leftwards as long these conditions are true, to remove trailing spaces
 	for i >= 0 && s[i] == ' ' {
 		i--
 	}
 	if i < 0 {
 		return "\n"
 	}
+	//mark where the last word ends i.e the last non-space character we meet
 	end := i
+
+	//move the i leftwards again but in this case until we meet an empty space
 	for i >= 0 && s[i] != ' ' {
 		i--
 	}
+	//slice one character after the space we found to one character after the last character
 	return s[i+1: end+1] + "\n"
 }
 
