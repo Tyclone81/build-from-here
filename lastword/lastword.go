e3447c4e03f8c0ed373e4130f4d4ada9e95fc589
vogero <ogerovictor81@gmail.com>
2026-08-04 17:12:24 +0300

    feat: implement lastword logic

--- lastword/lastword.go
+++ lastword/lastword.go
@@ -3,5 +3,22 @@ A word is a section of string delimited by spaces or by the start/end of the str
 package main
 import "fmt"
 func LastWord(s string) string{
-	
+	i := len(s)-1
+	for i >= 0 && s[i] == ' ' {
+		i--
+	}
+	if i < 0 {
+		return "\n"
+	}
+	end := i
+	for i >= 0 && s[i] != ' ' {
+		i--
+	}
+	return s[i+1: end+1] + "\n"
+}
+
+func main() {
+	fmt.Print(LastWord("this        ...       is sparta, then again, maybe    not"))
+	fmt.Print(LastWord(" lorem,ipsum "))
+	fmt.Print(LastWord(" "))
 }
