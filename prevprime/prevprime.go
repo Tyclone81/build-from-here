3ea306c682f2a690c3bcbcfe5be264893e1884e5
vogero <ogerovictor81@gmail.com>
2026-08-12 16:04:05 +0300

    feat: comments to elaborate on program logic

--- prevprime/prevprime.go
+++ prevprime/prevprime.go
@@ -3,9 +3,13 @@ If there are no primes inferior to the int passed as parameter the function shou
 package main
 import "fmt"
 func FindPrevPrime(nb int) int {
+	// least prime is 2
 	for i := nb; i >= 2; i-- {
+
+		//flag isPrime to track we are printing a prime number
 		isPrime := true
 		
+		// loop to confirm the previous numbers are either prime or not 
 		for j := 2; j * j <= i; j++ {
 			if i % j == 0 {
 				isPrime = false
@@ -16,6 +20,7 @@ func FindPrevPrime(nb int) int {
 			return i
 		}
 	}
+	//if no primes inferior to nb return 0
 	return 0
 }
 
