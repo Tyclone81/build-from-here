9b02d1286b9ca221633fcc71c027f72f264fb117
vogero <ogerovictor81@gmail.com>
2026-08-12 15:29:28 +0300

    feat: print reverse combination program logic

---
+++ revcomb/revcomb.go
@@ -0,0 +1,26 @@
+/* Write a program that prints in descending order on a single line all unique combinations of three different digits so that the first digit is greater than the second and the second is greater than the third.
+These combinations are separated by a comma and a space.*/
+
+package main
+import "github.com/01-edu/z01"
+func main() {
+	// flag fisrt to track we are printing the first combination
+	first := true
+
+	for a := 9; a >= 2; a-- {
+		for b := a-1; b >= 1; b-- {
+			for c := b-1; c >= 0; c-- {
+				if ! first{
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+				first = false
+				
+				z01.PrintRune(rune('0' + a))
+				z01.PrintRune(rune('0' + b))
+				z01.PrintRune(rune('0' + c))
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
