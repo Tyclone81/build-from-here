f223c4d72ba390de20cc51c691fe7a06be51d6ea
vogero <ogerovictor81@gmail.com>
2026-09-23 10:36:48 +0300

    feat: add the wordanatomy challenge requirements

---
+++ wordanatomy/wordanatomy.go
@@ -0,0 +1,8 @@
+/*
+  Write a function main.go:5 that takes a single word as a string and dissects it into its constituent anatomical components: its prefix, root, and suffix.
+  The function must return three string values:
+	 (prefix, root, suffix)
+ The function must recognize the following 10 prefixes:
+	 • "un", "re", "pre", "mis", "dis", "over", "under", "anti", "inter", "sub"
+  The function must recognize the following 10 suffixes:
+  • "ing", "ed", "er", "est", "ly", "ness", "ment", "tion", "able", "ful" */
